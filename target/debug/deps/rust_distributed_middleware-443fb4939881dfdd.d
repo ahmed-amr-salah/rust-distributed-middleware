7de65c22a50f316f92f965d1aae2316fa3f5a00c
Mario Ghaly <mariomamdouh@aucegypt.edu>
2024-11-03 05:52:47 +0200

    what is this

---
+++ target/debug/deps/rust_distributed_middleware-443fb4939881dfdd.d
@@ -0,0 +1,5 @@
+/home/mario/Documents/GitHub/rust-distributed-middleware/target/debug/deps/librust_distributed_middleware-443fb4939881dfdd.rmeta: src/main.rs
+
+/home/mario/Documents/GitHub/rust-distributed-middleware/target/debug/deps/rust_distributed_middleware-443fb4939881dfdd.d: src/main.rs
+
+src/main.rs:
