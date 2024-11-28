a963267366f83a0d90e8096b7e9b8701b7d47dc1
FreddyAmgad <78415690+FreddyAmgad@users.noreply.github.com>
2024-11-28 19:38:50 +0200

    Working Server

--- target/debug/deps/rust_distributed_middleware-a1839818db913f0b.d
+++
@@ -1,5 +0,0 @@
-/home/mario/Documents/GitHub/rust-distributed-middleware/target/debug/deps/librust_distributed_middleware-a1839818db913f0b.rmeta: src/main.rs
-
-/home/mario/Documents/GitHub/rust-distributed-middleware/target/debug/deps/rust_distributed_middleware-a1839818db913f0b.d: src/main.rs
-
-src/main.rs:
